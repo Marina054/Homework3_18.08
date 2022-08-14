cff509d121e817b24a74e8aa0a85255f8a2d9f1a
marina <litvinova_06@bk.ru>
2022-08-14 22:04:42 +0500

    work_1

---
+++ work_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// string Method4 (int count,string text)
+// {
+//     string result = string.Empty;
+//     for (int i=0; i<count;i++)
+//     {
+//         result = result+text;
+//     } 
+//     return result;
+// }
+// string res = Method4 (10, "asdf");
+// Console.WriteLine (res);
+
+
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+// int[]arr = {1,4,2,1,2};
+// for(int i=0; i< arr.Length/2;i++ )
+// {
+//     (arr[i], arr[arr.Length - (i+1)]) = (arr[arr.Length - (i+1)], arr[i]);
+// }
+// Console.WriteLine (string.Join (" ", arr));
+// Console.WriteLine ("НЕТ");
+
+
+
+
+// int[]arr = {1,2,8,2,1};
+// for(int i=0; i< arr.Length/2;i++ )
+// {
+//     (arr[i], arr[arr.Length - (i+1)]) = (arr[arr.Length - (i+1)], arr[i]);
+// }
+// Console.WriteLine (string.Join (" ", arr));
+// Console.WriteLine ("ДА");
+
+
+
+int[]arr = {2,3,4,3,2};
+for(int i=0; i< arr.Length/2;i++ )
+{
+    (arr[i], arr[arr.Length - (i+1)]) = (arr[arr.Length - (i+1)], arr[i]);
+}
+Console.WriteLine (string.Join (" ", arr));
+Console.WriteLine ("ДА");
